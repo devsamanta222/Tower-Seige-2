16d9ad4a08ff7bbc4bfb2e1e41cc8b3f3131f280
devsamanta222 <71442372+devsamanta222@users.noreply.github.com>
2020-12-31 19:15:08 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,43 @@
+class Box {
+  constructor(x,y,width,height) {
+    var options = {
+       friction: 0.3,
+       density: 0.8,
+       restitution: 0.2
+    }
+    
+    this.body = Bodies.rectangle(x,y,width,height,options);
+    this.width = width;
+    this.height = height;
+    this.Visiblity = 255;
+    World.add(world, this.body);
+  }
+  display(){
+
+    if(this.body.speed < 3){
+      var pos =this.body.position;
+    rectMode(CENTER);
+      rect(pos.x, pos.y, this.width, this.height);
+    
+      
+    }
+    else{
+     World.remove(world, this.body);
+     push();
+
+     this.Visiblity = this.Visiblity - 5;
+     //tint(255,this.Visiblity);
+     pop();
+     
+   }
+
+
+    
+    
+    
+   
+  }
+
+
+
+};
